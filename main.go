d9794e2ca0f9d60cd1a1df8a4140ee5f9dc36ca1
Ana Maria Palacio Henao <ampalaci@proteccion.com.co>
2024-03-20 13:45:34 -0500

    Siguiente commit

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/anapalacio0721/godesde0/variables"
 )
 
 func main() {
-	variables.MuestroEnteros()
+	estado, texto := variables.ConviertoaTexto1(45)
+	fmt.Println(estado)
+	fmt.Println(texto)
 
 }
