b2b57b2aa439569c45439a288173e79763e1d699
Ana Maria Palacio Henao <ampalaci@proteccion.com.co>
2024-03-27 13:38:50 -0500

    Siguiente Commit

--- main.go
+++ main.go
@@ -3,12 +3,33 @@ package main
 import (
 	"fmt"
 
-	"github.com/anapalacio0721/godesde0/variables"
+	ejercicios "github.com/anapalacio0721/godesde0/ejercicios"
 )
 
 func main() {
-	estado, texto := variables.ConviertoaTexto1(45)
+
+	/*estado, texto := variables.ConviertoaTexto1(45)
 	fmt.Println(estado)
+	fmt.Println(texto)*/
+	/*
+		if os := runtime.GOOS; os == "Linux." || os == "OS X" {
+			fmt.Printf("Esto no es Windows")
+		} else {
+			fmt.Println("Esto es Windows")
+
+		}
+
+		switch os := runtime.GOOS; os {
+		case "linux":
+			fmt.Println("Esto es Linux")
+		case "darwin":
+			fmt.Println("Esto es Darwin")
+		default:
+			fmt.Printf("%s \n", os)
+		}
+	*/
+	numero, texto := ejercicios.ConvNumerico("500")
+	fmt.Println(numero)
 	fmt.Println(texto)
 
 }
